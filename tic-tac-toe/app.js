89a089c528da08ec245df24faa6f5d29027a6b6f
redperezz <kaicruzz91@gmail.com>
2020-08-18 18:15:00 -0700

    created css file, added gameboard to html, working on td eventlisteners

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,5 +1,15 @@
+// TODO:
 // first move always starts with player 'X'
-
 // the app detects a win or a tie and displays an appropriate message
-
 // a button resets the game for a new round of gameplay
+
+
+let board = document.querySelectorAll("td");
+let clickForPlayerX = function(square) {
+  console.log(square);
+}
+
+board.forEach((square) => {
+  square.addEventListener('click', clickForPlayerX)
+})
+console.log(board);
